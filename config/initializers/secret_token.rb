7b673b169c575323f7b8e47da4455a9ff56fb5ff
Gabriel Reis <bielmooca@gmail.com>
2011-12-14 23:48:25 -0200

    first import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurnipPageObject::Application.config.secret_token = '64529ed04fb4f1b2eb5a1d7c89da435d4c0b8cd2e7ee0272c1bd0e9c177caf824715a988d93ff09e560111a8dd245a69a27617005b2e3cc6be34b17065eff852'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TurnipPageObject::Application.config.session_store :cookie_store, key: '_turnip-page-object_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TurnipPageObject::Application.config.session_store :active_record_store
